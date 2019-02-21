460e78976db35eb66614ad0de73f721f531fcbfd
Balek91 <antoinefaller.a@gmail.com>
2019-02-21 17:08:06 +0100

    unit test simulate ok

--- src/test/book.js
+++ src/test/book.js
@@ -126,11 +126,122 @@ describe('Test unitaire (simulation ok)', () => {
         nock.cleanAll()
     })
     it('should send http status 200 and send an array',done=>{
-        const scope = nock("http://localhost:8080")
+        let emptyBooks = {
+            books: []
+        }
+        nock("http://localhost:8080")
         .get('/book')
-        .reply(200,[])
-        console.log(scope);
-        done();
+        .reply(200,emptyBooks)
+            chai
+            .request('http://localhost:8080')
+            .get('/book')
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.books).to.be.a('array');
+                expect(res.body.books.length).to.equal(0);
+                done();
+            });
+    })
+    it('should for post send http status 200 and send an message',done=>{
+        let message = {
+            message: 'book successfully added'
+        }
+        nock("http://localhost:8080")
+        .post('/book')
+        .reply(200,message)
+            chai
+            .request('http://localhost:8080')
+            .post('/book')
+            .send({
+                "id": "55b7d315-1a5f-4b13-a665-c382a6c71756",
+                "title": "Oui-Oui contre Dominique Strauss-Kahn",
+                "years": "2015",
+                "pages": "650"
+            })
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.message).to.be.a('string');
+                expect(res.body.message).to.equal('book successfully added');
+                done();
+            });
+    })
+    it('should for put send http status 200 and send an message',done=>{
+        let message = {
+            message: 'book successfully updated'
+        }
+        nock("http://localhost:8080")
+        .put('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+        .reply(200,message)
+            chai
+            .request('http://localhost:8080')
+            .put('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+            .send({
+                "pages": "650"
+            })
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.message).to.be.a('string');
+                expect(res.body.message).to.equal('book successfully updated');
+                done();
+            });
+    })
+    it('should for delete send http status 200 and send an message',done=>{
+        let message = {
+            message: 'book successfully deleted'
+        }
+        nock("http://localhost:8080")
+        .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+        .reply(200,message)
+            chai
+            .request('http://localhost:8080')
+            .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.message).to.be.a('string');
+                expect(res.body.message).to.equal('book successfully deleted');
+                done();
+            });
+    })
+
+    it('should for get send http status 200 and send an message',done=>{
+        let data = {
+            message: 'book fetched',
+            book : {
+                'id': '0db0b43e-dddb-47ad-9b4a-e5fe9ec7c2a9',
+                'title': 'Coco raconte Channel 2',
+                'years': 1990,
+                'pages': 400
+            }
+        }
+        nock("http://localhost:8080")
+        .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+        .reply(200,data)
+            chai
+            .request('http://localhost:8080')
+            .delete('/book/55b7d315-1a5f-4b13-a665-c382a6c71756')
+            .end((err, res) => {
+                if (err) console.log(err);
+                expect(res).to.have.status(200);
+                expect(res.body).to.be.a('object');
+                expect(res.body.message).to.be.a('string');
+                expect(res.body.message).to.equal('book fetched');
+                expect(res.body.book).to.be.a('object');
+                expect(res.body.book.years).to.be.a('number');
+                expect(res.body.book.years).to.equal(1990);
+                expect(res.body.book.pages).to.be.a('number');
+                expect(res.body.book.pages).to.equal(400);
+                expect(res.body.book.title).to.be.a('string');
+                expect(res.body.book.title).to.equal('Coco raconte Channel 2');
+                done();
+            });
     })
 })
 
